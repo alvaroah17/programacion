731ceb057b6c2a1933a7c68d6af061fa449485dc
Álvaro <alvaroo.ah17@gmail.com>
2025-11-06 19:16:22 +0100

    EJERCICIO2 TERMINADO

--- practica3/src/ejercicio2.java
+++ practica3/src/ejercicio2.java
@@ -18,19 +18,30 @@ public class ejercicio2 {
                 array4x5[i][j]= sc.nextInt();
             }
         }
+        System.out.println("------------------------------------------------------------------");
         for (int i = 0; i < array4x5.length; i++) {
             sumaFilas=0;
             for (int j = 0; j < array4x5[i].length; j++) {
-                System.out.printf("%10s", array4x5[i][j]);
+                System.out.printf("|"+"%10s", array4x5[i][j]);
                 sumaFilas+=array4x5[i][j];
+                sumaTotal+=array4x5[i][j];//Forma de hacerlo que va recogiendo los valores de las Filas y Columnas. Sin inicialiarse a 0 a diferencia de sumaFIlas
             }
-            System.out.printf("%10s",sumaFilas);
+            System.out.printf("|"+"%10s","∑"+sumaFilas+"|");
             System.out.println();
+            System.out.println("|----------|----------|----------|----------|----------|---------|");
+           // sumaTotal+=sumaFilas; Forma de Hacerlo sumandole los valores que va recogiendo sumaFilas antes de inicializarse a 0 de nuevo
         }
-        for (int i = 0; i < array4x5[0].length; i++) {
-            for (int j = 0; j < array4x5.length; j++) {
-                
+        for (int j = 0; j < array4x5[0].length; j++) {
+            numMayorColumna=0;//Volvemos a poner la variable a 0 aunque arriba ya este iniciada para que recoja los valores de la nueva columna
+            for (int i = 0; i < array4x5.length; i++) {
+                if (array4x5[i][j] > numMayorColumna){
+                    numMayorColumna=array4x5[i][j];
+                }
             }
+            System.out.printf("|"+"%10s","∑"+ numMayorColumna);
         }
+        System.out.printf("%10s","|TOTAL="+sumaTotal+"|");
+        System.out.println();
+        System.out.println("------------------------------------------------------------------");
     }
 }
