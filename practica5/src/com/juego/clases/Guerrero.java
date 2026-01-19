2d2f8bd0ab656dd1ac60479ed11af78cb4feb6f4
Álvaro <alvaroo.ah17@gmail.com>
2026-01-19 14:28:41 +0100

    avance practica5

--- practica5/src/com/juego/clases/Guerrero.java
+++ practica5/src/com/juego/clases/Guerrero.java
@@ -1,19 +1,28 @@
 package com.juego.clases;
 
+import com.juego.habilidades.Habilidades;
+import com.juego.habilidades.curacion.CuracionPequenia;
+import com.juego.habilidades.guerrero.Espadazo;
+import com.juego.habilidades.guerrero.LanzamientoEspada;
 import com.juego.modelo.Estadistica;
 import com.juego.modelo.Personaje;
+
+import java.util.ArrayList;
 
 public class Guerrero implements Clase{
     Estadistica estadisticas;
 
-    public Guerrero (Estadistica estadisticas){
-        this.estadisticas=estadisticas;
+    public Guerrero (){
+
     }
 
 
     @Override
     public void agregarHabilidad(Personaje personaje) {
-
+        ArrayList<Habilidades> listaHabilidades = personaje.getListaHabilidades();
+        listaHabilidades.add(new Espadazo());
+        listaHabilidades.add(new LanzamientoEspada());
+        listaHabilidades.add(new CuracionPequenia());
     }
 
     @Override
