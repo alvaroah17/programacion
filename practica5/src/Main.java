2d3078b0b92672e211398fd6d3388758b440855f
Álvaro <alvaroo.ah17@gmail.com>
2026-01-26 21:55:03 +0100

    MAIN FINAL

--- practica5/src/Main.java
+++ practica5/src/Main.java
@@ -5,16 +5,5 @@ public class Main {
 
         Menu menu=new Menu();
         menu.Menu();
-
-      /*GestorPersonajes gestorPersonajes= new GestorPersonajes();
-       gestorPersonajes.crearNuevoPersonaje();
-       System.out.println("PERSONAJES CREADOS");
-       for (int i = 0; i < gestorPersonajes.getPersonajes().size(); i++) {
-           System.out.println(gestorPersonajes.getPersonajes().get(i).getNombre());
-       }
-
-       Combate combate1 = new Combate();
-       combate1.iniciarCombate(personaje1, personaje2);
-       */
     }
 }
