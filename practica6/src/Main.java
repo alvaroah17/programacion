f5376d5fda52650f4467e4e3851b1e650bfec7b1
Álvaro <alvaroo.ah17@gmail.com>
2026-02-02 14:24:26 +0100

    PRACTICA 6 EMPEZADA

---
+++ practica6/src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        Bloque1 bloque1=new Bloque1();
+        bloque1.mostrar3erEvento1();
+        bloque1.mostrarNombreVillanos2();
+
+    }
+}
