77bade88a8946a9f53ed54297c08764c3fe7f244
Álvaro <alvaroo.ah17@gmail.com>
2026-03-03 12:03:53 +0100

    ACTUALIZACION PRCT 7

--- practica7/src/model/Ciudades.java
+++ practica7/src/model/Ciudades.java
@@ -1,4 +1,16 @@
 package model;
 
 public class Ciudades {
+    private String nombre;
+    private int poblacion;
+    private String clima;
+    private int riesgo;;
+
+    public Ciudades(String nombre, int poblacion, String clima, int riesgo){
+        this.nombre=nombre;
+        this.poblacion=poblacion;
+        this.clima=clima;
+        this.riesgo=riesgo;
+    }
+
 }
--- practica7/src/utils/TxtHelper.java
+++ practica7/src/utils/TxtHelper.java
@@ -1,4 +1,29 @@
 package utils;
 
+import model.Ciudades;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TxtHelper {
+    public List<Ciudades> leerCiudades() {
+        List<Ciudades> ciudades=new ArrayList<>();
+        try {
+            List<String> lineas = Files.readAllLines(Paths.get("practica7\\Ficheros\\ciudades.txt"));
+            for (String linea : lineas) {
+                System.out.println(linea);
+                String [] s =linea.split(";");
+                Ciudades ciudades2=new Ciudades(s[0], Integer.parseInt(s[1]), s[2],Integer.parseInt(s[3]));
+                ciudades.add(ciudades2);
+
+            }
+        } catch (IOException e) {
+            System.out.println("No se ha podido abrir el fichero.");
+        }
+        return ciudades;
+    }
+
 }
