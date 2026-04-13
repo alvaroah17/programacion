68a5366007cfdd96081ded4b89b191256df892b5
Alvaro <aalihur186@g.educaand.es>
2026-04-13 14:30:12 +0200

    avance 2

--- practica8/src/rpg/logic/GestionMundo.java
+++ practica8/src/rpg/logic/GestionMundo.java
@@ -1,5 +1,41 @@
 package rpg.logic;
 
-public class GestionMundo {
+import rpg.dao.*;
+import rpg.model.Personaje;
 
+import java.util.ArrayList;
+
+public class GestionMundo {
+    private CiudadDAO ciudadDAO;
+    private HabilidadesDAO habilidadesDAO;
+    private InventarioDAO inventarioDAO;
+    private ItemDAO itemDAO;
+    private PersonajeDAO personajeDAO;
+    private RazaDAO razaDAO;
+
+
+    public GestionMundo(CiudadDAO ciudadDAO, HabilidadesDAO habilidadesDAO, InventarioDAO inventarioDAO, ItemDAO itemDAO, PersonajeDAO personajeDAO, RazaDAO razaDAO) {
+        this.ciudadDAO = ciudadDAO;
+        this.habilidadesDAO = habilidadesDAO;
+        this.inventarioDAO = inventarioDAO;
+        this.itemDAO = itemDAO;
+        this.personajeDAO = personajeDAO;
+        this.razaDAO = razaDAO;
+    }
+
+    public ArrayList<Personaje> cobroImpuestos(){
+
+    }
+
+    public void comprobarOro (Personaje personaje, int cantidadComprobada){
+        if (personaje.getOro()<cantidadComprobada){
+            //throw new SaldoInsuficienteException
+        }else {
+            personaje.setOro(personaje.getOro()-cantidadComprobada);
+        }
+    }
+
+    public void comprobarNivel (Personaje personaje){
+
+    }
 }
